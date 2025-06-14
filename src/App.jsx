979b150849db798a8192a7967aeff7cdb9647620
Ряховский Михаил <sunflowerlmao@vk.com>
2025-06-14 22:33:25 +0700

    Исправлен импорт ExchangeRatesPage для Docker-сборки

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegisterPage from './components/RegisterPage';
-import LoginPage from './components/LoginPage';
-import HomePage from './components/HomePage';
-import ProductPage from './components/ProductPage';
-import CartPage from './components/CartPage';
-import AdminDashboard from './components/AdminDashboard';
-import NotFound from './components/NotFound';
 import Navbar from './components/Navbar';
-import ExchangeRatesPage from './components/ExchangeRatesPage ';
+import HomePage from './components/HomePage';
+import CartPage from './components/CartPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import AdminPage from './components/AdminDashboard';
+import ExchangeRatesPage from './components/ExchangeRatesPage';
 
 function App() {
   return (
@@ -16,14 +14,12 @@ function App() {
       <Navbar />
       <div className="container mt-4">
         <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<HomePage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/cart" element={<CartPage />} />
-          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/admin" element={<AdminPage />} />
           <Route path="/exchange-rates" element={<ExchangeRatesPage />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
